52441f77f383d348be4f6467df82276cc58dd6a7
Rodney <rondineypatricio@gmail.com>
2026-04-02 14:18:59 -0300

    feat: database pool

--- app/db/database.py
+++ app/db/database.py
@@ -2,7 +2,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 from app.core.config import settings
 
-engine = create_engine(settings.DATABASE_URL)
+#pool de conexoes 
+engine = create_engine(
+    settings.DATABASE_URL,
+    pool_pre_ping=True,
+    pool_size=10,
+    max_overflow=20,
+    echo=settings.DEBUG
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 class Base(DeclarativeBase):
