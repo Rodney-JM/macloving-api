c0da82d7f87493f77210b6267c544e9e52c83c1a
Rodney <rondineypatricio@gmail.com>
2026-04-04 14:10:37 -0300

    bug: logging except middleware bug fixxed

--- app/middleware/logging.py
+++ app/middleware/logging.py
@@ -10,26 +10,34 @@ class RequestLoggingMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next) -> Response:
         request_id = str(uuid.uuid4())[:8]
         start = time.perf_counter()
-        
-        #vinculando o requestid ao contexto de log dessa requisição
+
         structlog.contextvars.clear_contextvars()
         structlog.contextvars.bind_contextvars(request_id=request_id)
-        
+
         try:
             response = await call_next(request)
+            status_code = response.status_code
+            response.headers["X-Request-ID"] = request_id
         except Exception as e:
-            duration_ms = round((time.perf_counter() - start) * 1000,2)    
-        
-        
+            status_code = 500
+            
+            logger.error(
+                "http_error",
+                method=request.method,
+                path=request.url.path,
+                error=str(e)
+            )
+            raise
+
+        duration_ms = round((time.perf_counter() - start) * 1000, 2)
+
         logger.info(
             "http_request",
             method=request.method,
             path=request.url.path,
-            status=response.status_code,
+            status=status_code,
             duration_ms=duration_ms,
             user_agent=request.headers.get("user-agent", "")
         )
-        
-        response.headers["X-Request-ID"] = request_id
-        
+
         return response
